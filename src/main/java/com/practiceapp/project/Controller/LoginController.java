a3855693f0a33482c2f429aefab70685826a759b
VaishaliBhaskar <111138424+VaishaliBhaskar@users.noreply.github.com>
2023-05-16 15:09:36 +0530

    Update LoginController.java

--- src/main/java/com/practiceapp/project/Controller/LoginController.java
+++ src/main/java/com/practiceapp/project/Controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+	//get login page
 	@GetMapping("/login")
 	public String loginPage() {
 		return "login";
