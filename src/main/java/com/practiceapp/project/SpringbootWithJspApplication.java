17c7b5ca97738464b5d0ced9c19932920bb2a967
Vaishali Bhaskar <vaishalibhaskar2015@gmail.com>
2023-05-16 14:12:40 +0530

    initial commit

---
+++ src/main/java/com/practiceapp/project/SpringbootWithJspApplication.java
@@ -0,0 +1,14 @@
+package com.practiceapp.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithJspApplication.class, args);
+	}
+
+}
+
---
+++ src/main/webapp/WEB-INF/JSP/login.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>login page</title>
+</head>
+<body>
+<form>
+<input type="text" name="userId" placeholder="User Name"/>
+<br><br/>
+<input type="text" name="password" placeholder="password ">
+</form>
+</body>
+</html>
